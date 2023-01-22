658989002111c27d04b613e5305aa828baf86a7b
Thaddaeus <thaddaeusotieno@gmail.com>
2023-01-22 13:18:40 +0300

    Todo list

--- script.js
+++ script.js
@@ -4,6 +4,18 @@
 //working with the dom
 //document.body.innerHTML = "Thaddaeus"
 //document.getElementsByTagName(P).innerHTML = "Thaddaeus";
+
+//creating variables
 let todo1 = 'Get grocaries';
 let todo2 = 'Wash car';
 let todo3 = 'Make dinner';
+//logging the varialble into the console.
+console.log(todo1);
+//operator used for type checking and returns the data type of the operand passed to it
+typeof 'todo1'
+// Data types.
+//number
+//strings
+
+//string concatination
+console.log('I am Thaddaeus' + 'From Kenya');
