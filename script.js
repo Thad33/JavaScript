5779a23b1c0b3c43e80dec4c21bbf201052e1f71
Thaddaeus <thaddaeusotieno@gmail.com>
2023-01-22 13:12:01 +0300

    Todo list

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //alert("Hello World")
-const date = new Date();
-console.log(date)
+//const date = new Date();
+//console.log(date)
 //working with the dom
 //document.body.innerHTML = "Thaddaeus"
-document.getElementsByTagName(P).innerHTML = "Thaddaeus";
+//document.getElementsByTagName(P).innerHTML = "Thaddaeus";
+let todo1 = 'Get grocaries';
+let todo2 = 'Wash car';
+let todo3 = 'Make dinner';
